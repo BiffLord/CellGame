842c2b10a266e946dfddf78324fc07bd441fa5df
Biff <saah1798t3l@gmail.com>
2025-12-18 21:18:28 -0500

    Added game loop

--- src/net/biff/Screen.java
+++ src/net/biff/Screen.java
@@ -5,19 +5,49 @@ import net.biff.organelles.Organelle;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
+import java.lang.Runnable;
 
-public class Screen extends JPanel {
-    private List<Organelle> organelles;
+public class Screen extends JPanel implements Runnable{
+    private final List<Organelle> organelles;
+    public Thread gameLoop;
+
     public Screen(List<Organelle> orgs){
         this.organelles = orgs;
+        gameLoop = new Thread(this);
+        gameLoop.start();
     }
 
     @Override
     protected void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        organelles.forEach(x -> {if (x.visible) {x.draw(g2d);}});
-        g2d.dispose();
-        g.dispose();
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            organelles.forEach(x -> {
+                if (x.visible) {
+                    x.draw(g2d);
+                }
+            });
+            g2d.dispose();
+            g.dispose();
+    }
+
+    @Override
+    public void run() {
+        while (gameLoop != null){
+            long startTime = System.nanoTime();
+            repaint();
+            long endTime = System.nanoTime();
+            long n;
+            long x;
+            final int FPS = 60;
+            if ((n = endTime-startTime) < (x = 1000000000/ FPS)){
+                try {
+                    synchronized (gameLoop) {
+                        gameLoop.wait(x/1000000-n/1000000);
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
--- src/net/biff/organelles/Mitochondrion.java
+++ src/net/biff/organelles/Mitochondrion.java
@@ -31,7 +31,7 @@ public class Mitochondrion extends Organelle{
 
     @Override
     public void draw(Graphics2D g2d) {
-        g2d.setStroke(stroke);
+        g2d.setStroke(smallStroke);
         g2d.setColor(new Color(152, 0,0));
         g2d.fill(hitbox);
         g2d.setColor(new Color(204, 0,0));
@@ -40,6 +40,6 @@ public class Mitochondrion extends Organelle{
 
     }
     private void matrixComponent(double x, double y){
-        matrix.add(new Ellipse2D.Double(centerX+x,centerY+y,10,25));
+        matrix.add(new Ellipse2D.Double(centerX+x,centerY+y,12,25));
     }
 }
