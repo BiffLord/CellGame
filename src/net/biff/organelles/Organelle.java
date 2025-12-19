52aaafa564c30ba7409ead6aa821fe138e3de461
Biff <saah1798t3l@gmail.com>
2025-12-18 21:18:28 -0500

    Added game loop

--- src/net/biff/organelles/Organelle.java
+++ src/net/biff/organelles/Organelle.java
@@ -6,7 +6,7 @@ import java.awt.geom.Ellipse2D;
 public abstract class Organelle {
     public Shape hitbox;
     public boolean visible = true;
-    public BasicStroke smallStroke = new BasicStroke(5.0f);
+    public BasicStroke smallStroke = new BasicStroke(7f);
     public BasicStroke stroke = new BasicStroke(10.0f);
 
     public abstract void draw(Graphics2D g2d);
